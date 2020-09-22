8ec1bf3a250193fa8ec007e854ffed4faf1a6db5
Jesse Livezey <jesse.livezey@gmail.com>
2020-09-22 14:49:02 -0400

    basic shifter example

---
+++ shifter_example/shifter_example.py
@@ -0,0 +1,33 @@
+import numpy as np
+from itertools import product
+from argparse import ArgumentParser
+from mpi4py import MPI
+
+
+# Parse arguments
+parser = ArgumentParser()
+parser.add_argument('data', type=str, help='The input h5 file')
+data = parser.data
+
+# Setup MPI sizes
+comm = MPI.COMM_WORLD
+size = comm.size
+rank = comm.rank
+
+n_channels = 64
+n_time = 100
+
+def analysis(file, ch_idx, time_idx):
+    # Load some data
+    # Do some analysis
+    # Save some results
+    return
+
+# Select tasks for this rank
+args = list(product([data], np.arange(n_channels), np.arange(n_time)))
+args = np.array(args, dtype=object)
+my_args = np.array_split(args, size)[rank]
+
+# Run this rank's tasks
+for arg in args:
+    analysis(*arg)
---
+++ shifter_example/shifter_example.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+#SBATCH --qos=regular
+#SBATCH --constraint=knl
+#SBATCH --nodes=200
+#SBATCH --time=02:00:00
+#SBATCH --image=docker:yourname/image:tag
+
+# 200 nodes for 2 hours using shifter image
+
+cores=2
+export MKL_NUM_THREADS=$cores
+export OMP_NUM_THREADS=$cores
+export HDF5_USE_FILE_LOCKING=FALSE
+
+# Running a job for 4 subjects that use 50 nodes each and 1000 tasks each
+#    file    nodes tasks             use shifter       local script       script options
+srun -o s1.o -N 50 -n 1000 -c $cores shifter python -u shifter_example.py /global/cfs/cdirs/m2043/jlivezey/S1.h5 &
+
+srun -o s2.o -N 50 -n 1000 -c $cores shifter python -u shifter_example.py /global/cfs/cdirs/m2043/jlivezey/S2.h5 &
+
+srun -o s3.o -N 50 -n 1000 -c $cores shifter python -u shifter_example.py /global/cfs/cdirs/m2043/jlivezey/S3.h5 &
+
+srun -o s4.o -N 50 -n 1000 -c $cores shifter python -u shifter_example.py /global/cfs/cdirs/m2043/jlivezey/S4.h5 &
+
+wait
